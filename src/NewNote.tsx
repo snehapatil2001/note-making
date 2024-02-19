9a6b919a80f7607e2a79772f41e202f00fe2f638
Sneha <snehapatildeepali@gmail.com>
2024-02-19 16:59:36 +0530

    Feat: Use LocalStorage and Note creation logic.

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,10 +1,14 @@
+import { NoteData } from "./App";
 import { NoteForm } from "./NoteForm";
 
-export function NewNote() {
+type NewNoteProps = {
+	onSubmit: (data: NoteData) => void
+}
+export function NewNote({ onSubmit }: NewNoteProps) {
 	return (
-	<>
-		<h1 className="mb-4">New Note</h1>
-		<NoteForm />
-	</>
+		<>
+			<h1 className="mb-4">New Note</h1>
+			<NoteForm onSubmit={onSubmit} />
+		</>
 	)
 }
