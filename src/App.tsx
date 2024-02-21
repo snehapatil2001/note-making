1275799adc6c1e14e9fff3c1a043e6953a9a694a
Sneha <snehapatildeepali@gmail.com>
2024-02-21 13:45:59 +0530

    Feat: Note Edit Page.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { useMemo } from "react";
 import { v4 as uuidV4 } from "uuid";
 import { NoteList } from "./NoteList";
 import { NoteLayout } from "./NoteLayout";
+import { Note } from "./Note";
+import { EditNote } from "./EditNote";
 
 export type Note = {
 	id: string
@@ -54,6 +56,17 @@ function App() {
 		})
 	}
 
+	function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+		setNotes(prevNotes => {
+			return prevNotes.map(note => {
+				if (note.id === id) {
+					return { ...note, ...data, tagIds: tags.map(tag => tag.id) }
+				} else {
+					return note
+				}
+			})
+		})
+	}
 	function addTag(tag: Tag) {
 		setTags(prev => [...prev, tag])
 	}
@@ -70,8 +83,14 @@ function App() {
 					/>
 				} />
 				<Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-					<Route index element={<h1>Show</h1>} />
-					<Route path="edit" element={<h1>Edit</h1>} />
+					<Route index element={<Note />} />
+					<Route path="edit" element={
+						<EditNote
+							onSubmit={onUpdateNote}
+							onAddTag={addTag}
+							availableTags={tags}
+						/>
+					} />
 				</Route>
 				<Route path="*" element={<Navigate to="/" />} />
 			</Routes>
--- src/NoteLayout.tsx
+++ src/NoteLayout.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, useParams } from "react-router-dom";
+import { Navigate, Outlet, useOutletContext, useParams } from "react-router-dom";
 import { Note } from "./App";
 
 type NoteLayoutProps = {
@@ -11,4 +11,8 @@ export function NoteLayout({ notes }: NoteLayoutProps) {
 	if (note == null) return <Navigate to="/" replace />
 
 	return <Outlet context={note} />
+}
+
+export function useNote() {
+	return useOutletContext<Note>()
 }
