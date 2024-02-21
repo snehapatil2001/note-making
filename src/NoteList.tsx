3fbb6ac302dd7093503ea7261a93b94a99d4ad2a
Sneha <snehapatildeepali@gmail.com>
2024-02-21 19:19:08 +0530

    Feat: Tags modal and their closing logic.

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,4 +1,4 @@
-import { Badge, Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
+import { Badge, Button, Card, Col, Form, Modal, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
 import { Tag } from "./App";
@@ -14,9 +14,16 @@ type NoteListProps = {
 	availableTags: Tag[]
 	notes: SimplifiedNote[]
 }
+type EditTagsModalProps = {
+	show: boolean
+	availableTags: Tag[]
+	handleClose: () => void
+}
+
 export function NoteList({ availableTags, notes }: NoteListProps) {
 	const [selectedTags, setSelectedTags] = useState<Tag[]>([])
 	const [title, setTitle] = useState("")
+	const [editTagsModalIsOpen, setEditTagsModalIsOpen] = useState(false)
 
 	const filteredNotes = useMemo(() => {
 		return notes.filter((note) => {
@@ -42,7 +49,12 @@ export function NoteList({ availableTags, notes }: NoteListProps) {
 						<Link to="/new">
 							<Button variant="primary">Create</Button>
 						</Link>
-						<Button variant="outline-secondary">Edit Tags</Button>
+						<Button
+							onClick={() => setEditTagsModalIsOpen(true)}
+							variant="outline-secondary"
+						>
+							Edit Tags
+						</Button>
 					</Stack>
 				</Col>
 			</Row>
@@ -83,6 +95,7 @@ export function NoteList({ availableTags, notes }: NoteListProps) {
 					</Col>
 				))}
 			</Row>
+			<EditTagsModal show={editTagsModalIsOpen} handleClose={() => setEditTagsModalIsOpen(false)} availableTags={availableTags} />
 		</>
 	)
 }
@@ -99,19 +112,43 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
 				<Stack
 					gap={2}
 					className="align-items-center justify-content-center h-100"
-					>
-						<span className="fs-5">{title}</span>
-						{tags.length > 0 && (
-							<Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
-								{tags.map(tag => (
-									<Badge className="text-truncate" key={tag.id}>
-										{tag.label}
-									</Badge>
-								))}
-							</Stack>
-						)}
-					</Stack>
+				>
+					<span className="fs-5">{title}</span>
+					{tags.length > 0 && (
+						<Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
+							{tags.map(tag => (
+								<Badge className="text-truncate" key={tag.id}>
+									{tag.label}
+								</Badge>
+							))}
+						</Stack>
+					)}
+				</Stack>
 			</Card.Body>
 		</Card>
 	)
+}
+
+function EditTagsModal({ availableTags, handleClose, show }: EditTagsModalProps) {
+	return <Modal show={show} onHide={handleClose}>
+		<Modal.Header closeButton>
+			<Modal.Title>Edit Tags</Modal.Title>
+		</Modal.Header>
+		<Modal.Body>
+			<Form>
+				<Stack gap={2}>
+					{availableTags.map(tag => (
+						<Row key={tag.id}>
+							<Col>
+								<Form.Control type="text" value={tag.label} />
+							</Col>
+							<Col xs="auto">
+								<Button variant="outline-danger">&times;</Button>
+							</Col>
+						</Row>
+					))}
+				</Stack>
+			</Form>
+		</Modal.Body>
+	</Modal>
 }
